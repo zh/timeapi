26b1d0fe279dcb1dabb7b8f0dc4fe7f8742583df
Matt Pennig <matt@Fatso.local>
2009-08-27 22:24:26 -0700

    fixing offsets

--- timeapi.rb
+++ timeapi.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'chronic'
+require 'date'
 
 module TimeAPI
   PST = -8
@@ -11,6 +12,8 @@ module TimeAPI
   MDT = -6
   CDT = -5
   EDT = -4
+  UTC = 0
+  GMT = 0
   
   class App < Sinatra::Default
   
@@ -22,19 +25,37 @@ module TimeAPI
       erb :index
     end
     
+    get '/favicon.ico' do
+      ''
+    end
+    
     get '/:zone' do
       zone = params[:zone].upcase
-      offset = TimeAPI::const_get(zone) * 60 * 60
+      offset = TimeAPI::const_get(zone)
       
-      (Time.now.utc - offset).iso8601
+      Time.new.utc.to_datetime.new_offset(Rational(offset,24)).to_s
     end
     
     get '/:zone/:time' do
       zone = params[:zone].upcase
-      offset = TimeAPI::const_get(zone) * 60 * 60
+      offset = TimeAPI::const_get(zone)
       
-      Chronic.parse(params[:time], :now=>Time.now.utc - offset).iso8601
+      Chronic.parse(
+        params[:time], :now=>Time.new.utc
+      ).to_datetime.new_offset(Rational(offset,24)).to_s
     end
   
+  end
+end
+
+class Time
+  def to_datetime
+    # Convert seconds + microseconds into a fractional number of seconds
+    seconds = sec + Rational(usec, 10**6)
+
+    # Convert a UTC offset measured in minutes to one measured in a
+    # fraction of a day.
+    offset = Rational(utc_offset, 60 * 60 * 24)
+    DateTime.new(year, month, day, hour, min, seconds, offset)
   end
 end
