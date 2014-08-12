b5c37cb8e028ef84dddb3028a42cca44bfc94b7d
stoyan <stoyan@midx.jp>
2014-08-13 01:51:30 +0900

    Implementing 'callback' and 'format'; short timezones names; bugfixes

--- timeapi.rb
+++ timeapi.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 require 'chronic'
 require 'date'
 require 'cgi'
+require 'json'
+require 'active_support/time'
 
 ENV['RACK_ENV'] ||= "development"
 ENV['TIMEAPI_MIME'] ||= "text/plain"
@@ -17,63 +19,138 @@ module TimeAPI
     end
 
     helpers do
-      # convert zone to offset
-      def z2o(zone)
-        offsets = { "PST" => -8, "MST" => -7, "CST" => -6, "EST" => -5, 
-                    "PDT" => -7, "MDT" => -6, "CDT" => -5, "EDT" => -4,
-                    "UTC" =>  0, "GMT" => 0 }
-        offsets[zone ? zone.upcase : "UTC"]
-      end  
+      ZoneOffset = {
+        'A' => +1,
+        'ADT' => -3,
+        'AKDT' => -8,
+        'AKST' => -9,
+        'AST' => -4,
+        'B' => +2,
+        'BST' => +1,
+        'C' => +3,
+        'CDT' => -5,
+        'CEDT' => +2,
+        'CEST' => +2,
+        'CET' => +1,
+        'CST' => -6,
+        'D' => +4,
+        'E' => +5,
+        'EDT' => -4,
+        'EEDT' => +3,
+        'EEST' => +3,
+        'EET' => +2,
+        'EST' => 'EST',
+        'F' => +6,
+        'G' => +7,
+        'GMT' => 'GMT',
+        'H' => +8,
+        'HADT' => -9,
+        'HAST' => -10,
+        'I' => +9,
+        'IST' => +1,
+        'JST' => +9,
+        'K' => +10,
+        'L' => +11,
+        'M' => +12,
+        'MDT' => -6,
+        'MSD' => +4,
+        'MSK' => +3,
+        'MST' => -7,
+        'N' => -1,
+        'O' => -2,
+        'P' => -3,
+        'PDT' => -7,
+        'PST' => -8,
+        'Q' => -4,
+        'R' => -5,
+        'S' => -6,
+        'T' => -7,
+        'U' => -8,
+        'UTC' => 'UTC',
+        'V' => -9,
+        'W' => -10,
+        'WEDT' => +1,
+        'WEST' => +1,
+        'WET' => 0,
+        'X' => -11,
+        'Y' => -12,
+        'Z' => 0
+      }
+
+      def callback
+        (request.params['callback'] || '').gsub(/[^a-zA-Z0-9_]/, '')
+      end
+	
+      def prefers_json?
+        (request.accept.first || '').downcase == 'application/json'
+      end
+  
+      def json?
+        prefers_json? \
+          || /\.json$/.match((params[:zone] || '').downcase) \
+          || /\.json$/.match((params[:time] || '').downcase)
+      end
+	
+      def jsonp?
+        json? && !callback.empty?
+      end
+
+      def format
+        format = (request.params.select { |k,v| v.empty? }.first || [nil]).first \
+          || request.params['format'] \
+          || (jsonp? ? '%B %d, %Y %H:%M:%S GMT%z' : '')
+        CGI.unescape(format).gsub('\\', '%')
+      end
+
+      def parse(zone='UTC', time='now')
+        zone = zone.gsub(/\.json$/, '').upcase
+        offset = ZoneOffset[zone] || Integer(zone)
+        time = time \
+          .gsub(/\.json$/, '') \
+          .gsub(/^at /, '') \
+          .gsub(/(\d)h/, '\1 hours') \
+          .gsub(/(\d)min/, '\1 minutes') \
+          .gsub(/(\d)m/, '\1 minutes') \
+          .gsub(/(\d)sec/, '\1 seconds') \
+          .gsub(/(\d)s/, '\1 seconds')
+      
+        if prefers_json?
+          response.headers['Content-Type'] = 'application/json'
+        else
+          response.headers['Content-Type'] = ENV['TIMEAPI_MIME']
+        end
+
+        fmt = format.empty? ? "%Y-%m-%d %H:%M:%S %Z" : format
+        time = Chronic.parse(CGI.unescape(time))
+        time = DateTime.parse((time ? time : Time.now).in_time_zone(offset).to_s).strftime(fmt)
+        time = json? ? { 'dateString' => time }.to_json : time
+        time = jsonp? ? callback + '(' + time + ');' : time
+        time
+      end
     end  
   
     get '/' do
       erb :index
     end
 
-    post '/' do
-      throw :halt, [400, "Bad request, missing 'dt' parameter"] unless params[:dt]
-      content_type ENV['TIMEAPI_MIME']
-      offset = z2o(params[:zone])
-      Time.new.utc.to_datetime.new_offset(Rational(offset, 24)).to_s
-    end
-    
     get '/favicon.ico' do
       ''
     end
-    
-    get '/:zone' do
-      content_type ENV['TIMEAPI_MIME']
-      offset = z2o(params[:zone])
-      Time.new.utc.to_datetime.new_offset(Rational(offset,24)).to_s
-    end
-    
-    get '/:zone/:time' do
-      offset = z2o(params[:zone])
-      result = Chronic.parse(CGI.unescape(params[:time]),:now=>Time.new.utc.to_datetime.new_offset(Rational(offset,24)))
-      throw :halt, [400, "Bad request"] unless result
-      content_type ENV['TIMEAPI_MIME']
-      result.to_datetime.new_offset(Rational(offset,24)).to_s
+
+    post '/' do
+      throw :halt, [400, "Bad request, missing 'dt' parameter"] unless params[:dt]
+      parse(params[:zone], params[:dt])
     end
 
+    get '/:zone/?' do
+      parse(params[:zone])
+    end
+    
+    get '/:zone/:time/?' do
+      parse(params[:zone], params[:time])
+    end
+  
     # start the server if ruby file executed directly
     run! if app_file == $0  
-  end
-end
-
-class Time
-  def to_datetime
-    # Convert seconds + microseconds into a fractional number of seconds
-    seconds = sec + Rational(usec, 10**6)
-
-    # Convert a UTC offset measured in minutes to one measured in a
-    # fraction of a day.
-    offset = Rational(utc_offset, 60 * 60 * 24)
-    DateTime.new(year, month, day, hour, min, seconds, offset)
-  end
-end
-
-class DateTime
-  def to_datetime
-    self
   end
 end
